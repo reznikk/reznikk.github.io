d46bfda3ed0bcbe777ce71dbd77449e36092875b
reznikk <reznik.git@gmail.com>
2017-04-01 12:05:13 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "rezniksergey@mail.ua";
+$sitename = "Название сайта";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nEmail: $email \nСообщение: $message";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
