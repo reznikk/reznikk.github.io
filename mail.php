86691660579e0fa458bc95954ab5139986ca4749
reznikk <reznik.git@gmail.com>
2017-04-01 12:27:32 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,12 +1,20 @@
 <?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$text = $_POST["text"];
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$text = htmlspecialchars($text);
+$name = urldecode($name);
+$email = urldecode($email);
+$text = urldecode($text);
+$name = trim($name);
+$email = trim($email);
+$text = trim($text);
 
-$recepient = "rezniksergey@mail.ua";
-$sitename = "Название сайта";
 
-$name = trim($_POST["name"]);
-$email = trim($_POST["email"]);
-$text = trim($_POST["text"]);
-$message = "Имя: $name \nEmail: $email \nСообщение: $message";
-
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+if (mail("example@mail.ru", "Заявка с сайта", "Имя:".$name.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
